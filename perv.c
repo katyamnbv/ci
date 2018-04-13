8d3862d37644ded9b6d4342fe6eac03443b5d0e1
katyamnbv <32410006+katyamnbv@users.noreply.github.com>
2018-04-14 01:57:14 +0300

    1404

---
+++ perv.c
@@ -0,0 +1,10 @@
+void push(struct node* head, int data) {
+	struct node* newNode = (struct node*) malloc(sizeof(struct node));
+	newNode->data = data;
+	newNode->next = head;
+	head = newNode;
+}
+void push_test() {
+	List head = build();
+	push(head, 1);
+}
